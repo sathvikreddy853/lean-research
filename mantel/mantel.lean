7ab058ee158064fc62d17f8ce2ca9fa38eedca76
Sathvik Reddy <sathvikreddy853@gmail.com>
2026-04-18 13:55:38 +0530

    [Update]: Update Theorem Proofs

--- mantel/mantel.lean
+++ mantel/mantel.lean
@@ -30,15 +30,15 @@ Formally: v ∈ N(u) ↔ uv ∈ E.
 -/
 lemma mem_neighborFinset_iff_adj {u v : V} :
   v ∈ G.neighborFinset u ↔ G.Adj u v := by
-  sorry
+  simp [SimpleGraph.mem_neighborFinset]
 
 /--
 The size of the neighborhood of a vertex equals its degree.
 Formally: |N(v)| = deg(v).
 -/
-lemma card_neightFinset_eq_degree (v : V) :
+lemma card_neighborFinset_eq_degree (v : V) :
   (G.neighborFinset v).card = G.degree v := by
-  sorry
+  simp [SimpleGraph.card_neighborFinset_eq_degree]
 
 /--
 In a triangle-free graph, if uv is an edge, then there is no vertex w
@@ -48,7 +48,9 @@ Formally: uv ∈ E ⇒ ¬∃ w, uw ∈ E ∧ vw ∈ E.
 lemma no_common_neighbor_of_edge
   (htri : TriangleFree (G := G)) {u v w : V} (huv : G.Adj u v) :
   ¬ (G.Adj u w ∧ G.Adj v w) := by
-  sorry
+  intro h
+  rcases h with ⟨huw, hvw⟩
+  exact htri u v w huv hvw huw
 
 /--
 In a triangle-free graph, if uv is an edge, then the neighborhoods
@@ -56,28 +58,44 @@ of u and v are disjoint.
 Formally: uv ∈ E ⇒ N(u) ∩ N(v) = ∅.
 -/
 lemma neighborFinsets_disjoint_for_edge
-  (htri : TriangleFree (G := G)) {u v w : V} (huv : G.Adj u v) :
-  (G.neighborFinset u) ∩ (G.neighborFinset v) = ∅ := by
-  sorry
+  (htri : TriangleFree (G := G)) {u v : V} (huv : G.Adj u v) :
+  Disjoint (G.neighborFinset u) (G.neighborFinset v) := by
+  rw [disjoint_left]
+  intro w huw hvw
+  rw [mem_neighborFinset_iff_adj] at huw hvw
+  exact no_common_neighbor_of_edge htri huv ⟨huw, hvw⟩
 
 /--
 In a triangle-free graph, for any edge uv, the sum of degrees is bounded
 by the number of vertices.
 Formally: uv ∈ E ⇒ deg(u) + deg(v) ≤ |V|.
 -/
-lemma sum_of_degree_le_card_of_vertex_set
+lemma sum_of_degree_le_card_of_vset
   (htri : TriangleFree (G := G)) {u v : V} (huv : G.Adj u v) :
   G.degree u + G.degree v ≤ Fintype.card V := by
-  sorry
+    rw [← card_neighborFinset_eq_degree u]
+    rw [← card_neighborFinset_eq_degree v]
+    have hdisj : Disjoint (G.neighborFinset u) (G.neighborFinset v) :=
+      neighborFinsets_disjoint_for_edge htri huv
+    rw [← Finset.card_union_of_disjoint hdisj]
+    apply Finset.card_le_univ
 
 /--
 (Handshaking Lemma)
 The sum of degrees of all vertices equals twice the number of edges.
 Formally: ∑_v deg(v) = 2|E|.
 -/
-lemma handshake_lemma :
+lemma handshaking_lemma :
   (∑ v : V, G.degree v) = 2 * G.edgeFinset.card := by
-  sorry
+  exact SimpleGraph.sum_degrees_eq_twice_card_edges G
+
+/--
+The sum of squares of degrees is bounded by the product of the number of vertices and edges.
+Formally: ∑_v deg(v)^2 ≤ |V||E|
+-/
+lemma sum_sq_degree_le_card_vset_mul_card_edges :
+  (∑ v : V, (G.degree v)^2 ) ≤ G.edgeFinset.card * Fintype.card V := by
+  apply?
 
 /--
 In a triangle-free graph, the number of edges satisfies:
